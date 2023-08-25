312d4b3d993c137a3ceb9bad0adaa0b8dcc027f5
Uday Kiran <officialudaykiran@gmail.com>
2023-08-25 20:41:01 +0530

    deployable version🕸️

--- config.js
+++ config.js
@@ -4,8 +4,7 @@ module.exports = {
     port: process.env.PORT || 8000,
     jwt: 'MY#SECRET@CODE$',
     mongoose: {
-        // url: 'mongodb+srv://udaykirancodes:udaykirancodes@cluster0.ip9n0c4.mongodb.net/chat-app?retryWrites=true&w=majority',
-        url: 'mongodb://localhost:27017/expo-app?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',
+        url: process.env.MONGOURL,
         options: {
             useNewUrlParser: true,
             useUnifiedTopology: true,
